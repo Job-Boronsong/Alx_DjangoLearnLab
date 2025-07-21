91e3c8a1b4dccf6fa6b6c8688a823deab0c3bb77
Job Boronsong <jboronsong@gmail.com>
2025-07-21 00:23:25 +0000

    commit changes

--- django-models/bookshelf/admin.py
+++ django-models/bookshelf/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book
 
 # Register the Book model
-admin.site.register(Book)
+# admin.site.register(Book)
 
 
 class BookAdmin(admin.ModelAdmin):
