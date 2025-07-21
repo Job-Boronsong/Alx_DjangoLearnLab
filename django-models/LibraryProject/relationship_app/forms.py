6d8e947b7117f3386a1144fc5a6831deea11a850
Job Boronsong <jboronsong@gmail.com>
2025-07-21 03:19:25 +0000

    update forms.py with BookForm

--- django-models/LibraryProject/relationship_app/forms.py
+++ django-models/LibraryProject/relationship_app/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
