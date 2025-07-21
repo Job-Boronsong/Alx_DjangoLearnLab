da10013e2bb3ec04771bb513cd3f005c2fdf8d9e
Job Boronsong <jboronsong@gmail.com>
2025-07-21 01:40:31 +0000

    commt

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -22,9 +22,13 @@ def get_books_in_library(library_name):
     return library.books.all()
 
 # 3. Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    librarian = Librarian.objects.get(library__name=library_name)
-    return librarian
+library_name = "Central Library"  # Replace with a real library name from your database
+from relationship_app.models import Library, Librarian
+
+library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library)
+
+print(f"Librarian for {library_name}: {librarian.name}")
 
 
 # --- Sample usage ---
