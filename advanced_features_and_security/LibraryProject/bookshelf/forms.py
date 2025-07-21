563d32110ab289cac766d4e373f3459ea3c3d26a
Job Boronsong <jboronsong@gmail.com>
2025-07-21 08:50:28 +0000

    update

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,3 +1,8 @@
 from django import forms
+
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100)
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Email Address')
