eae4e8d7aca11220e5a413846747c1ff753df4ee
Job Boronsong <jboronsong@gmail.com>
2025-07-21 08:23:10 +0000

    update

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 
