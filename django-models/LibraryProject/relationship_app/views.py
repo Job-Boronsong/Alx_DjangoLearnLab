ac4b4777263197a0c5e38e26339917fd40b3bb60
Job Boronsong <jboronsong@gmail.com>
2025-07-21 03:22:48 +0000

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,6 +13,8 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseForbidden
 from .models import Book
 from .forms import BookForm
+from django.contrib.auth.decorators import permission_required
+
 
 def is_superuser(user):
     return user.is_superuser
