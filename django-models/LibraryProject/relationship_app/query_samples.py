132ea0e3eba41800f0e5bc02f6543f7344d2f57a
Job Boronsong <jboronsong@gmail.com>
2025-07-21 02:00:46 +0000

    commit new changes

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,13 +8,9 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-author_name = "John Do"  # You can change the name as needed
-author = Author.objects.get(name=author_name)
-books_by_author = Book.objects.filter(author=author)
-
-print(f"Books by {author_name}:")
-for book in books_by_author:
-    print(f"- {book.title}")
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 # 2. List all books in a library
 def get_books_in_library(library_name):
@@ -22,13 +18,9 @@ def get_books_in_library(library_name):
     return library.books.all()
 
 # 3. Retrieve the librarian for a library
-library_name = "Central Library"  # Replace with a real library name from your database
-from relationship_app.models import Library, Librarian
-
-library = Library.objects.get(name=library_name)
-librarian = Librarian.objects.get(library=library)
-
-print(f"Librarian for {library_name}: {librarian.name}")
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
 
 
 # --- Sample usage ---
