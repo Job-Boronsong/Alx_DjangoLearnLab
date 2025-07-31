56f7f542934cb973995ac07bcb83233347201ddb
Job Boronsong <jboronsong@gmail.com>
2025-07-31 09:52:44 +0000

    commit LL

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()          # Fetch all books
     serializer_class = BookSerializer      # Serialize with BookSerializer
+
+# New ViewSet for full CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()              # All books
+    serializer_class = BookSerializer          # Use our serializer
