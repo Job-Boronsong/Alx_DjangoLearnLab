b82711a40ada35588ba1d844b181e8748af1288a
Job Boronsong <jboronsong@gmail.com>
2025-08-07 18:26:05 +0000

    commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,20 @@
-from rest_framework import generics, permissions
+# api/views.py
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from .filters import BookFilter
+
+
+class BookListCreate(generics.ListCreateAPIView):
+   
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = BookFilter # Use the custom filter class
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
 
 # List all books or create a new book
 class BookListCreateView(generics.ListCreateAPIView):
