2d3e5c6209b005622246c094c76129d1387b4b58
Job Boronsong <jboronsong@gmail.com>
2025-08-11 16:56:13 +0000

    commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,47 +1,115 @@
 # blog/views.py
-from django.shortcuts import get_object_or_404
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django import forms
+
 from .models import Post
-from .forms import PostForm
+
+
+# ------------------------
+# AUTHENTICATION VIEWS
+# ------------------------
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful!")
+            return redirect("posts-list")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Logged in successfully!")
+            return redirect("posts-list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "You have been logged out.")
+    return redirect("login")
+
+
+@login_required
+def profile_view(request):
+    return render(request, "registration/profile.html")
+
+
+# ------------------------
+# BLOG POST CRUD VIEWS
+# ------------------------
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/posts_list.html'   # your template
-    context_object_name = 'posts'
-    ordering = ['-published_date']
-    paginate_by = 10  # optional
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-created_at"]
+
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    # success_url uses get_absolute_url by default
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
 
     def form_valid(self, form):
-        # assign the logged-in user as author before saving
         form.instance.author = self.request.user
+        messages.success(self.request, "Post created successfully!")
         return super().form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, "Post updated successfully!")
+        return super().form_valid(form)
 
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('posts-list')
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("posts-list")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Post deleted successfully!")
+        return super().delete(request, *args, **kwargs)
 
     def test_func(self):
         post = self.get_object()
