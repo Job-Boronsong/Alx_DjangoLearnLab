80b28dae1a8635444481a41aa1f9890bd4d35007
Job Boronsong <jboronsong@gmail.com>
2025-08-11 17:00:31 +0000

    commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views  # ✅ now you can use views.register_view, views.profile_view
 from .views import (
+    register_view, profile_view,
     PostListView, PostDetailView, PostCreateView,
     PostUpdateView, PostDeleteView
 )
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('profile/', views.profile_view, name='profile'),
+    path('register/', register_view, name='register'),
+    path('profile/', profile_view, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('posts/', PostListView.as_view(), name='posts-list'),
