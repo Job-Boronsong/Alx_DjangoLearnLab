0ee1f013aba1cafdc79bf620ab609789c9f1ccd5
Job Boronsong <jboronsong@gmail.com>
2025-08-11 17:30:28 +0000

    commit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,6 @@
 # blog/forms.py
 from django import forms
-from .models import Post
+from .models import Post, Comment  # Make sure Comment exists in models.py
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,20 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'placeholder': 'Post title'}),
             'content': forms.Textarea(attrs={'rows': 10, 'placeholder': 'Write your post here...'}),
         }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'rows': 3,
+                'placeholder': 'Write your comment here...'
+            }),
+        }
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content or content.strip() == "":
+            raise forms.ValidationError("Comment cannot be empty.")
+        return content
