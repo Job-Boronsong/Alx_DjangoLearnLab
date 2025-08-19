f3295f390249b43cbb3c76d9317caa0d2e0ea637
Job Boronsong <jboronsong@gmail.com>
2025-08-19 06:17:10 +0000

    update posts/views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related("author").all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # Filter by author id if needed (?author=<id>)
+    filterset_fields = ["author"]
+    # Search title/content (?search=foo)
+    search_fields = ["title", "content"]
+    ordering_fields = ["created_at", "updated_at", "title"]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.select_related("author", "post").all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # Filter by post id (?post=<id>) and/or author (?author=<id>)
+    filterset_fields = ["post", "author"]
+    search_fields = ["content"]
+    ordering_fields = ["created_at", "updated_at"]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
