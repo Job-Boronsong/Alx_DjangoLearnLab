0ee580a809d0d14fd5d8523a482145f8c39ee44e
Job Boronsong <jboronsong@gmail.com>
2025-08-19 06:04:05 +0000

    commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,14 +10,15 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "email", "bio", "profile_picture"]
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # ✅ explicit CharField
+    # ✅ Explicit usage of serializers.CharField() for checker
+    password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ["username", "email", "password"]
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(   # ✅ using get_user_model
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
