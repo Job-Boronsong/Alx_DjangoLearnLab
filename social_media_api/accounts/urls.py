4572abc3091c1c0ea5178f0ede7a25b54311b593
Job Boronsong <jboronsong@gmail.com>
2025-08-19 06:56:11 +0000

    update accounts/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
 ]
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+
+urlpatterns = router.urls
