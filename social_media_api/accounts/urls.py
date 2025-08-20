173960647f7d5372abdb5bc522f3b4115f63af0b
Job Boronsong <jboronsong@gmail.com>
2025-08-20 04:16:32 +0000

    added url paths to accounts/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,7 +6,9 @@ from .views import UserViewSet
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
-    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/", ProfileView.as_view(), name="profile"),path("follow/<int:user_id>/", views.FollowUserView.as_view(), name="follow-user"),
+    path("unfollow/<int:user_id>/", views.UnfollowUserView.as_view(), name="unfollow-user"),
+    
 ]
 
 
